50ed3db33d6d3e6242c5cf3fbc28db078bb22243
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-03-10 22:59:41 +0300

    Create run.sh

---
+++ firstboot/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+grub-install
+grub-mkconfig -o /boot/grub/grub.cfg
+
+boot_a_md5sum=$(md5sum /dev/disk/by-partlabel/boot_a | cut -d' ' -f1)
+boot_b_md5sum=$(md5sum /dev/disk/by-partlabel/boot_b | cut -d' ' -f1)
+checksum_file_data=$(cat /firstboot/checksum | tr -d '\n')
+if [[ $checksum_file_data = *"$boot_a_md5sum"* ]]; then
+  target_partition="boot_a"
+else
+	if [[ $checksum_file_data = *"$boot_b_md5sum"* ]]; then
+	  target_partition="boot_b"
+	else
+	  echo "Error: MD5 checksum of /dev/disk/by-partlabel/boot_a and boot_b does not match any checksum in /firstboot/checksum"
+	  exit 1
+	fi
+fi
+
+dd if=/firstboot/boot.img of=/dev/disk/by-partlabel/$target_partition
+
+rm -rf /firstboot
+systemctl disable nabu-grub
+systemctl mask nabu-grub
+reboot -f

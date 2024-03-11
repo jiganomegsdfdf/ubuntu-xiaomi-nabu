701146b8eb46a2f3a4b262a95b9337e00e8208d9
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-03-11 14:01:57 +0300

    Update nabu-rootfs_build.sh

--- nabu-rootfs_build.sh
+++ nabu-rootfs_build.sh
@@ -87,9 +87,9 @@ sed --in-place 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' 
 sed --in-place 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' rootdir/etc/default/grub
 
 #create fstab!
-echo "PARTLABEL=linux / ext4 errors=remount-ro,x-systemd.growfs,x-systemd.device-timeout=10s,nofail 0 1
-PARTLABEL=pmos / ext4 errors=remount-ro,x-systemd.growfs,x-systemd.device-timeout=10s,nofail 0 1
-PARTLABEL=esp /boot/efi vfat umask=0077 0 1" | tee rootdir/etc/fstab
+echo "PARTLABEL=linux / ext4 errors=remount-ro,x-systemd.growfs,x-systemd.device-timeout=5s,nofail 0 1" >> rootdir/etc/fstab
+echo "PARTLABEL=pmos / ext4 errors=remount-ro,x-systemd.growfs,x-systemd.device-timeout=5s,nofail 0 1" >> rootdir/etc/fstab
+echo "PARTLABEL=esp /boot/efi vfat umask=0077 0 1" >> rootdir/etc/fstab
 
 mkdir rootdir/var/lib/gdm
 touch rootdir/var/lib/gdm/run-initial-setup

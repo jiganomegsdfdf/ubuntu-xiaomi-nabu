024eec686e6e4e96a73937c3991ae45f7fb73d63
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-04-06 22:53:53 +0300

    Update nabu-Kernel_build.sh

--- nabu-Kernel_build.sh
+++ nabu-Kernel_build.sh
@@ -6,8 +6,8 @@ echo 'CONFIG_CMDLINE="quiet splash"' >> ./arch/arm64/configs/sm8150.config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig sm8150.config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 mkdir ../linux-xiaomi-nabu/boot
-cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$2.0-sm8150
-cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$2.0-sm8150
+cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$1.0-sm8150
+cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$1.0-sm8150
 rm -rf ../linux-xiaomi-nabu/lib
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=../linux-xiaomi-nabu modules_install
 rm ../linux-xiaomi-nabu/lib/modules/**/build
